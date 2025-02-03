399bf9a6acca607635b86c0af89aa173f4fce625
Ma'rifatu Ambiya <123404337+2log2@users.noreply.github.com>
2025-02-03 17:54:19 +0700

    Add files via upload

---
+++ tugaspertemuan1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author marif
+ */
+public class tugaspertemuan1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        System.out.println("Hallo World Nama Saya Ma'rifatu Ambiya NIM 2300822, Saya Senang Belajar Java.");
+    }
+}
